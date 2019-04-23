df602f71bcb03174d6beb9b8f3f287d067c5be15
samusenko.d <dima@samusenka.com>
2019-04-23 18:27:00 +0300

    Implement loading feature in button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'c-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loading = false;
   title = 'angular-ui-kit';
+  ngOnInit() {
+    setInterval(() => {
+      this.loading = !this.loading;
+    }, 2000);
+  }
 }
--- src/app/directives/button.directive.ts
+++ src/app/directives/button.directive.ts
@@ -1,10 +1,57 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, OnDestroy, OnChanges, SimpleChanges, Optional, Inject } from '@angular/core';
+import { CUiKitConfigArgs, CUiKitConfig } from '../config/c-ui-kit-config';
+
+export type cButtonValue = boolean | number | undefined | null;
 
 @Directive({
   selector: '[cButton]'
 })
-export class ButtonDirective {
+export class ButtonDirective implements OnInit, OnChanges {
+  @Input('cButton') loading: cButtonValue;
+  @Input() disabled: boolean;
 
-  constructor() { }
+  el: any;
+  spinner: HTMLSpanElement;
 
+  constructor(el: ElementRef, @Inject(CUiKitConfig) @Optional() config: CUiKitConfigArgs) {
+    this.el = el;
+    this.spinner = this.createSpinner();
+    el.nativeElement.appendChild(this.spinner);
+  }
+
+  ngOnInit() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.loading) {
+      this.updateButton(changes.loading.previousValue);
+    }
+
+    if (changes.disabled) {
+      this.updateDisabled();
+    }
+  }
+
+  createSpinner(): HTMLSpanElement {
+    const span = document.createElement('span');
+    span.classList.add('cui-button-spinner');
+    return span;
+  }
+
+  private updateDisabled(): void {
+    this.el.disabled = this.disabled;
+  }
+
+  private updateButton(previousValue: cButtonValue): void {
+    const loading: boolean = !!this.loading;
+    const wasLoading: boolean = !!previousValue;
+
+    if (!loading) {
+      this.spinner.classList.toggle('show');
+      return this.updateDisabled();
+    }
+
+    if (!wasLoading) {
+      this.spinner.classList.toggle('show');
+    }
+  }
 }
