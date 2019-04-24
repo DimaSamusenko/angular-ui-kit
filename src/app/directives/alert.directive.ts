ad02185c041dfa3c23aae30ec5e2bf1cfc14ae5a
samusenko.d <dima@samusenka.com>
2019-04-24 15:58:05 +0300

    Close btn in alert

--- src/app/directives/alert.directive.ts
+++ src/app/directives/alert.directive.ts
@@ -1,12 +1,31 @@
-import {Directive, ElementRef, Inject, Input, Optional} from '@angular/core';
+import {Directive, ElementRef, Inject, Input, OnInit, Optional} from '@angular/core';
 import { CUiKitConfigArgs, CUiKitConfig } from '../config/c-ui-kit-config';
 
 @Directive({
   selector: '[cAlert]'
 })
-export class AlertDirective {
-  @Input() color: string;
+export class AlertDirective implements OnInit {
+  @Input() dismissible: boolean;
 
-  constructor(el: ElementRef, @Inject(CUiKitConfig) @Optional() config: CUiKitConfigArgs) { }
+  closeBtn: HTMLSpanElement;
+
+  constructor(el: ElementRef, @Inject(CUiKitConfig) @Optional() config: CUiKitConfigArgs) {
+    if (this.dismissible === true) {
+      this.closeBtn = this.createCloseBtn();
+      this.closeBtn.addEventListener('click', this.hide);
+    }
+  }
+
+  ngOnInit(): void {}
+
+  createCloseBtn(): HTMLSpanElement {
+    const span = document.createElement('span');
+    span.classList.add('cui-alert-close');
+    return span;
+  }
+
+  hide() {
+    this.closeBtn.removeEventListener('click', this.hide);
+  }
 
 }
